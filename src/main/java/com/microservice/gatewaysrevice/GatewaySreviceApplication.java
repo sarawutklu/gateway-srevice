b3ceccec1ded3054e722d38c850a980712a76d9d
DESKTOP-VUB0V1K\KO <sarawut@jpwall.com>
2021-07-12 23:32:40 +0700

    inti gateway

---
+++ src/main/java/com/microservice/gatewaysrevice/GatewaySreviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.gatewaysrevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySreviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaySreviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/gatewaysrevice/GatewaySreviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.gatewaysrevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySreviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
